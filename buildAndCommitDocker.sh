40c6872abda35c82dffc74a01f3577c6c3068782
boaz-betser-hpe <boaz.betser@hpe.com>
2016-01-24 22:49:13 +0200

    Update buildAndCommitDocker.sh

--- buildAndCommitDocker.sh
+++ buildAndCommitDocker.sh
@@ -5,3 +5,4 @@ logFile=build.log
 docker build . | tee $logFile
 containerName=`cat $logFile|grep "Successfully built"|gawk '{ print $3 }'`
 echo docker commit $containerName  $imageName:$version
+docker run --name appPulseJenkins -p 80:8080 -p 50000:50000 $containerName
