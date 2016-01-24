707b8d99cfe0a8156b6297f47b071ffc08f9b62a
boaz-betser-hpe <boaz.betser@hpe.com>
2016-01-24 23:42:53 +0200

    Update buildAndCommitDocker.sh

--- buildAndCommitDocker.sh
+++ buildAndCommitDocker.sh
@@ -3,6 +3,7 @@ imageName=AppPulseJenkins
 version=1.0
 logFile=build.log
 cp -R /NAS_ROOT/products/TPS/TPS/Latest/SUN/JDK/1.8.0_51/linux64 resources/SUN/JDK/1.8.0_51/linux64
+cp -R /NAS_ROOT/products/TPS/TPS/Latest/Apache/maven/3.0.3/multi-platform resources/Apache/maven/3.0.3/multi-platform
 docker build . | tee $logFile
 containerName=`cat $logFile|grep "Successfully built"|gawk '{ print $3 }'`
 echo docker commit $containerName  $imageName:$version
