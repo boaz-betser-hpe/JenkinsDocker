7dbb71fd735ccb4c34b28d07a1d9f11beb05e604
boaz-betser-hpe <boaz.betser@hpe.com>
2016-01-27 16:52:23 +0200

    Update runNewJenkins.sh

--- runNewJenkins.sh
+++ runNewJenkins.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-echo "Usage: startDocker.sh externalPort"
+echo "Usage: startDocker.sh <externalPort>"
 externalPort=$1
+if [ "$externalPort" = "" ]
+    then
+    externalPort=80
+fi
 imageName="AppPulseJenkins$externalPort"
 version=1.0
 logFile=build.log
@@ -8,4 +12,4 @@ JENKINS_INTERNAL_HTTP_PORT=9999
 containerName=`cat $logFile|grep "Successfully built"|gawk '{ print $3 }'`
 cmd="docker run -h mydev.devdomain.com -e JENKINS_OPTS="--httpPort=$JENKINS_INTERNAL_HTTP_PORT" --name $imageName -p $externalPort:$JENKINS_INTERNAL_HTTP_PORT $containerName"
 echo $cmd
-`$cmd`
+`$cmd` &
