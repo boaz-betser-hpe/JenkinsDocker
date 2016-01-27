c2f7c0c36cb7cf2f996e2da89a83de2241a07607
boaz-betser-hpe <boaz.betser@hpe.com>
2016-01-27 16:54:48 +0200

    Update stopJenkins.sh

--- stopJenkins.sh
+++ stopJenkins.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
-echo "Usage: startJenkins.sh <extrenalPort>"
-imageName="AppPulseJenkins$1"
+echo "Usage: stopJenkins.sh <extrenalPort>"
+externalPort=$1
+if [ "$externalPort" = "" ]
+    then
+    externalPort=80
+fi
+imageName="AppPulseJenkins$externalPort"
 cmd="docker stop $imageName"
 echo $cmd
 `$cmd`
