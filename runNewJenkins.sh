401b69e8374b86632f2224efcca3d32bbdb3e951
Boaz Betser <boaz.betser@hpe.com>
2016-02-02 12:14:06 +0200

    Fix script

--- runNewJenkins.sh
+++ runNewJenkins.sh
@@ -12,4 +12,5 @@ JENKINS_INTERNAL_HTTP_PORT=9999
 containerName=`cat $logFile|grep "Successfully built"|gawk '{ print $3 }'`
 cmd="docker run -d -h `hostname`.`hostname -d` -e JENKINS_OPTS="--httpPort=$JENKINS_INTERNAL_HTTP_PORT" --name $imageName -p 80$externalPort:8080 -p $externalPort:$JENKINS_INTERNAL_HTTP_PORT $containerName"
 echo $cmd
-`$cmd` 
+$cmd
+./startJenkins.sh $externalPort 
