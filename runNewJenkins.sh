8281533bb0d8ef959737bf38f856e3ca3fe95bda
boaz-betser-hpe <boaz.betser@hpe.com>
2016-03-06 10:21:04 +0200

    Update runNewJenkins.sh

--- runNewJenkins.sh
+++ runNewJenkins.sh
@@ -11,7 +11,7 @@ logFile=build.log
 JENKINS_HOSTNAME=`hostname -f`
 JENKINS_INTERNAL_HTTP_PORT=9999
 containerName=`cat $logFile|grep "Successfully built"|gawk '{ print $3 }'`
-cmd="docker run -d -h $JENKINS_HOSTNAME --add-host mydev.devdomain.com:127.0.0.1 -e PS1=JenkinsDocker_$externalPort> -e JENKINS_OPTS=--httpPort=$JENKINS_INTERNAL_HTTP_PORT -e externalPort=$externalPort --name $imageName -p 80$externalPort:8080 -p 90$externalPort:9090 -p $externalPort:$JENKINS_INTERNAL_HTTP_PORT $containerName"
+cmd="docker run -d -h $JENKINS_HOSTNAME --add-host mydev.devdomain.com:127.0.0.1 -e PS1=JenkinsDocker_$externalPort> -e JENKINS_OPTS=--httpPort=$JENKINS_INTERNAL_HTTP_PORT -e externalPort=$externalPort --name $imageName -p 80$externalPort:8080 -p 90$externalPort:9090 -p 30$externalPort:30$externalPort -p $externalPort:$JENKINS_INTERNAL_HTTP_PORT $containerName"
 echo $cmd
 $cmd
 ./startJenkins.sh $externalPort
